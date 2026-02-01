d126fd88b0f7808ae81f1f95286d2eafa906a878
pepsi z <64223175+hideyosh1@users.noreply.github.com>
2026-02-01 12:39:07 -0600

    wine: deprecate winePackages and wineWowPackages

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13320,9 +13320,13 @@ with pkgs;
       }
     );
 
-  winePackages = recurseIntoAttrs (winePackagesFor (config.wine.build or "wine32"));
+  winePackages =
+    lib.warn "winePackages is deprecated. Please use 'wine64Packages' or 'wineWow64Packages' instead."
+      (recurseIntoAttrs (winePackagesFor (config.wine.build or "wine32")));
   wine64Packages = recurseIntoAttrs (winePackagesFor "wine64");
-  wineWowPackages = recurseIntoAttrs (winePackagesFor "wineWow");
+  wineWowPackages = lib.warn "wineWowPackages is deprecated. Please use 'wine64Packages' or 'wineWow64Packages' instead." (
+    recurseIntoAttrs (winePackagesFor "wineWow")
+  );
   wineWow64Packages = recurseIntoAttrs (winePackagesFor "wineWow64");
 
   wine = winePackages.full;
